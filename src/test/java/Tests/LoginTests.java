a7e964400d4959932b566dc9a7198157a8a333f0
Nkosi <njceles@gmail.com>
2025-06-18 19:25:31 +0200

    creqted the first test

--- src/test/java/Tests/LoginTests.java
+++ src/test/java/Tests/LoginTests.java
@@ -9,9 +9,10 @@ import static Constants.AppConstants.EXPECTED_LOGIN_PAGE_TITLE;
 public class LoginTests extends BaseTest {
 
     @Test
-    public void verifyLoginDisplayed() {
+    public void verifyLoginDisplayed() throws InterruptedException {
         String Actual_title = loginPage.getLoginPageTile();
         Assert.assertEquals(Actual_title, EXPECTED_LOGIN_PAGE_TITLE, "Title does not match");
+        Thread.sleep(2000); // Sleep for 2 seconds to observe the title
     }
 
 
