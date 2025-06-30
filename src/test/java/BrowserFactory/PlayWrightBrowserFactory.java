25f05fbd828976d4027601038735f49f7e0aab8e
Malibongwe <ap.mshumpela@gmail.com>
2025-06-30 14:37:03 +0200

    modified browser factory

--- src/test/java/BrowserFactory/PlayWrightBrowserFactory.java
+++ src/test/java/BrowserFactory/PlayWrightBrowserFactory.java
@@ -1,4 +1,24 @@
 package BrowserFactory;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.util.Properties;
+
 public class PlayWrightBrowserFactory {
+
+    private BrowserContext browserContext;
+    private Page page;
+    Browser browser;
+    Properties prop;
+
+    public void initBrowser(Properties
+                                    prop) {
+        String browserName = prop.getProperty("browser").trim();
+        Playwright playwright = Playwright.create();
+
+
+
 }
