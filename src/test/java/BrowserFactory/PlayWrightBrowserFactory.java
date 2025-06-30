c6ebb93a50dda087fdf0cee9313203f4ebc65a2b
Malibongwe <ap.mshumpela@gmail.com>
2025-06-30 14:52:23 +0200

    Created the Switch for the init method

--- src/test/java/BrowserFactory/PlayWrightBrowserFactory.java
+++ src/test/java/BrowserFactory/PlayWrightBrowserFactory.java
@@ -14,11 +14,26 @@ public class PlayWrightBrowserFactory {
     Browser browser;
     Properties prop;
 
-    public void initBrowser(Properties
-                                    prop) {
+    public void initBrowser(Properties prop) {
         String browserName = prop.getProperty("browser").trim();
         Playwright playwright = Playwright.create();
-
-
-
+        switch (browserName.toLowerCase()) {
+            case "chromium":
+                browser = playwright.chromium().launch();
+                break;
+            case "chrome":
+                browser = playwright.chromium().launch();
+                break;
+            case "firefox":
+                browser = playwright.firefox().launch();
+                break;
+            case "webkit":
+                browser = playwright.webkit().launch();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+    }
 }
